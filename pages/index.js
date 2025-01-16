3b327410c6f36cb527c6f9fcf53b88694cba9aa9
Rafael Teobaldo <78392365+rafateobaldo@users.noreply.github.com>
2025-01-16 16:23:53 +0000

    adding scripts `lint:check`and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Aqui é o Brasil 🇧🇷</h1>
+  return <h1>Aqui é o Brasil 🇧🇷</h1>;
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
